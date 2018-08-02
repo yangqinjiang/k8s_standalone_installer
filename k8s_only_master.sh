7c9f46c014994a486962ca2baddf872d1341f008
yangqinjiang <531553280@qq.com>
2018-08-02 17:47:10 +0800

    master的三大组件安装

--- k8s_only_master.sh
+++ k8s_only_master.sh
@@ -8,7 +8,6 @@ set -o pipefail
 master_inip='192.168.0.4' #本机内网IP
 hostname='instance-u9ukr10q' #主机名称
 
-
 echo -e "\033[42;37m-----------正在安装单机版的k8s组件-----------\033[0m"
 #echo -e "\033[44;37m解压文件...\033[0m" >&2
 #tar xf kubernetes-server-linux-amd64.tar.gz
