b37a7e2b69fd38112e9acd02826d639a6d661e09
yangqinjiang <531553280@qq.com>
2018-08-03 15:33:57 +0800

    pull istio的images

--- istio_docker_image.sh
+++ istio_docker_image.sh
@@ -25,17 +25,17 @@ docker pull istio/mixer:1.0.0
 docker tag istio/mixer:1.0.0 gcr.io/istio-release/mixer:1.0.0
 
 echo -e "\033[42;37m-------6, istio/pilot:1.0.0-------\033[0m"
-docker istio/pilot:1.0.0
+docker pull istio/pilot:1.0.0
 docker tag istio/pilot:1.0.0 gcr.io/istio-release/pilot:1.0.0
 
 echo -e "\033[42;37m-------7, istio/citadel:1.0.0-------\033[0m"
-docker istio/citadel:1.0.0
+docker pull istio/citadel:1.0.0
 docker tag istio/citadel:1.0.0 gcr.io/istio-release/citadel:1.0.0
 
 echo -e "\033[42;37m-------8, istio/servicegraph:1.0.0-------\033[0m"
-docker istio/servicegraph:1.0.0
+docker pull istio/servicegraph:1.0.0
 docker tag istio/servicegraph:1.0.0 gcr.io/istio-release/servicegraph:1.0.0
 
 echo -e "\033[42;37m-------9, istio/sidecar_injector:1.0.0-------\033[0m"
-docker istio/sidecar_injector:1.0.0
+docker pull istio/sidecar_injector:1.0.0
 docker tag istio/sidecar_injector:1.0.0 gcr.io/istio-release/sidecar_injector:1.0.0
